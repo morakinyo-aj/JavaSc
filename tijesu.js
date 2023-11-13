3946d622e4a63c22b2bf5ad9161df042361b549b
morakinyo-aj <146096620+morakinyo-aj@users.noreply.github.com>
2023-11-13 13:35:16 +0100

    Add files via upload

---
+++ tijesu.js
@@ -0,0 +1,34 @@
+// const correctPin = 2364;
+// const accBalance = 1000;
+// let active = true;
+
+// while(active==true){
+//     const name = prompt("Enter your name: ");
+//     const pin = prompt("Enter your pin: ");
+//     if (pin ==correctPin) {
+//         alert("Welcome to AccessBank "+ name);
+//         const withdrawalAmount = prompt("Enter how much you want to withdraw: ")
+//         if (withdrawalAmount<accBalance){
+//             alert("Take your cash of "+withdrawalAmount);
+//         }
+//         else{
+//             alert("Insufficient funds!!!")
+//         }
+//         const newTransaction = confirm("Do you want to perform another transaction")
+//         if(newTransaction==true){
+//             active = true;
+//         }
+//         else{
+//             active = false;
+//         }
+//     }
+
+    
+        
+
+//     else{
+//         alert("Incorrect Pin");
+//     }
+
+// }
+
